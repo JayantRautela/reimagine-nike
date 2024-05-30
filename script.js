58f02c0ce15e48318a8fdeaa01a9a3bc245983bd
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2024-05-30 22:42:07 +0530

    added files

---
+++ script.js
@@ -0,0 +1,26 @@
+const timeline1 = gsap.timeline();
+
+timeline1.from(".navbar .heading", {
+    opacity: 0,
+    delay: 0.4,
+    duration: 0.8,
+    y: 40
+})
+
+timeline1.from(".navbar-right p", {
+    opacity: 0,
+    x: 50,
+    duration: 0.4,
+    stagger: 0.4
+})
+
+timeline1.from(".hero-section img", {
+    opacity: 0,
+    duration: 0.5
+})
+
+timeline1.from(".first", {
+    opacity: 0,
+    y: -50,
+    duration: 1
+})
