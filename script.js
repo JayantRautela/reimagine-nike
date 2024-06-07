2286a2e328debd586be906441f1ca42fa4c19c39
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2024-06-07 22:43:57 +0530

    Adding animations

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
 // //animations for page 1.
+function animatePage1(){
+    const timeline1 = gsap.timeline();
 
-// function animatePage1(){
-//     const timeline1 = gsap.timeline();
+    timeline1.from(".navbar .heading", {
+        opacity: 0,
+        delay: 0.4,
+        duration: 0.8,      
+        y: 40
+    })
 
-//     timeline1.from(".navbar .heading", {
-//         opacity: 0,
-//         delay: 0.4,
-//         duration: 0.8,      
-//         y: 40
-//     })
+    timeline1.from(".navbar-right p", {
+        opacity: 0,
+        x: 50,
+        duration: 0.4,
+        stagger: 0.4
+    })
 
-//     timeline1.from(".navbar-right p", {
-//         opacity: 0,
-//         x: 50,
-//         duration: 0.4,
-//         stagger: 0.4
-//     })
+    timeline1.from(".hero-section img", {   
+    opacity: 0,
+    duration: 0.5
+    })
 
-//     timeline1.from(".hero-section img", {   
-//     opacity: 0,
-//     duration: 0.5
-//     })
+    timeline1.from(".first", {  
+    opacity: 0,
+    y: -50,
+    duration: 1
+    })
+}
 
-//     timeline1.from(".first", {  
-//     opacity: 0,
-//     y: -50,
-//     duration: 1
-//     })
-// }
-
-// animatePage1();
+animatePage1();
 
 
 // //animations for page2.
-// gsap.from(".page2 h1", {
-//     y: -50,
-//     scrollTrigger: {
-//         trigger: ".page2 h1",
-//         scroller: "body"
-//     }
-// })
+gsap.from(".page2 h1", {
+    y: -50,
+    scrollTrigger: {
+        trigger: ".page2 h1",
+        scroller: "body"
+    }
+})
 
 
-// gsap.to(".page2 .wrapper .image-container", {   
-//     x: -995,
-//     scrollTrigger: {
-//         trigger: ".page2",
-//         scroller: "body",
-//         start: "top 0%",
-//         end: "top -150%",
-//         // markers: true,
-//         scrub: 1,
-//         pin: true
-//     }
-// })
+gsap.to(".page2 .wrapper .image-container", {   
+    x: -995,
+    scrollTrigger: {
+        trigger: ".page2",
+        scroller: "body",
+        start: "top 0%",
+        end: "top -150%",
+        // markers: true,
+        scrub: 1,
+        pin: true
+    }
+})
